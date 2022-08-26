d35b14b2a905d6e79d6bfbbc979c9b977f40c168
andr-ew <andrewcshike@gmail.com>
2022-08-26 16:29:35 -0500

    set notes from data

--- reversesea.lua
+++ reversesea.lua
@@ -12,13 +12,6 @@ engine.name = 'PolySub'
 pattern_time = include 'lib/pattern_time_reverse'
 pat = pattern_time.new()
 
---grid data structure, an integer which will be treated as binary data
---  IMPORTANT: an ineger in lua may only store 64 bits. so a key limitation of this method
---             is that only 64 keys of the grid can be stored.
---             in this script, we're only using the left half of a 128 grid
---  initialized as 0 (all keys off)
-grid_data = 0
-
 --converstion between x,y coordinates (1-based) & bit index (0-based)
 
 function xy_to_bit(x, y, width, height)
@@ -50,25 +43,50 @@ end
 function binary_toggle(data, n, z) --not using this one, but it's a handy referece!
     return data ~ (1 << bit)
 end
+--compare two datasets, run a function for each bit that has changed between the two sets
+function binary_difference(new_data, old_data, fn_added, fn_removed)
+    local changed = new_data ~ old_data
+    for bit = 0,63 do
+        if binary_get(changed, bit) > 0 then
+            if binary_get(new_data, bit) > 0 then
+                fn_added(bit)
+            else
+                fn_removed(bit)
+            end
+        end
+    end
+end
+
+
+--grid data structure, an integer which will be treated as binary data
+--  IMPORTANT: an ineger in lua may only store 64 bits. so a key limitation of this method
+--             is that only 64 keys of the grid can be stored.
+--             in this script, we're only using the left half of a 128 grid
+--  initialized as 0 (all keys off)
+grid_data = 0
+
+--update grid data, and play notes *based on the change of data*
+function set_grid_data(new_data)
+    local old_data = grid_data
+
+    binary_difference(new_data, old_data, note_on, note_off)
+    
+    grid_data = new_data
+    grid_is_dirty = true
+end
 
 --grid input
 function g.key(x, y, z)
-    local old_data = grid_data
     local new_data
 
     local bit = xy_to_bit(x, y)
     if z>0 then
-        new_data = binary_set(old_data, bit)
-
-        note_on(bit)
+        new_data = binary_set(grid_data, bit)
     else
-        new_data = binary_clear(old_data, bit)
-        
-        note_off(bit)
+        new_data = binary_clear(grid_data, bit)
     end
-
-    grid_data = new_data
-    grid_is_dirty = true
+    
+    set_grid_data(new_data)
 end
 
 -- grid drawing & render loop
@@ -85,20 +103,16 @@ function grid_redraw()
   
     g:refresh()
 end
-
 grid_is_dirty = true
-do
-    local fps = 30
-    clock.run(function()
-        while true do
-            clock.sleep(1/fps)
-            if grid_is_dirty then
-                grid_is_dirty = false
-                grid_redraw()
-            end
+clock.run(function()
+    while true do
+        clock.sleep(1/30)
+        if grid_is_dirty then
+            grid_is_dirty = false
+            grid_redraw()
         end
-    end)
-end
+    end
+end)
 
 --pythagorean major pent, ref: https://en.wikipedia.org/wiki/Pythagorean_tuning 
 scale = { 1/1, 9/8, 81/64, 3/2, 27/16 }
@@ -109,7 +123,7 @@ function note_on(bit)
     local oct = (bit//#scale)
     local deg = (bit % #scale) + 1
     local ratio = scale[deg]
-    local hz = 110 * 2^(oct - 3) * ratio
+    local hz = 110 * 2^(oct - 5) * ratio
 
     engine.start(id, hz)
 end
